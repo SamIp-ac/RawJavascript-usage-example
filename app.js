6f9367d9939af8f66a008d6eda796f5fe4e939d5
samip-ac <i60996395@gmail.com>
2023-02-26 22:44:51 +0800

    Try to make it into api

---
+++ app.js
@@ -0,0 +1,27 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+const osmd = require('opensheetmusicdisplay.js');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/upload', (req, res) => {
+  const dataUrl = req.body.dataUrl;
+  const buffer = Buffer.from(dataUrl.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+  const reader = new osmd.FileStream(buffer);
+  const loader = new osmd.MusicXmlParser();
+  const score = loader.load(reader);
+  const osmd = new osmd.OpenSheetMusicDisplay('osmdCanvas');
+  osmd.setSheetMusic(score);
+  const svg = osmd.renderToSVG();
+  res.json({ svg });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ call_function.js
@@ -0,0 +1,4 @@
+import {handleFileSelect} from "./fileSelectAndLoadOSMD.js";
+
+var x = handleFileSelect('RawJavascript-usage-example-modify-version/resources/Sonata no 4 in D majo.xml');
+console.log(x)
